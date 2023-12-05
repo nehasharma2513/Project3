6d07644bd21c8c804c01eedc58d570ecdb73d1d0
Valentyna K <uzarevich@ukr.net>
2023-12-05 14:32:11 -0500

    skeleton for app with chartsjs simple chart

---
+++ static/js/app.js
@@ -0,0 +1,39 @@
+
+// Available routes:
+// let booksURL = '/api/v1.0/books'
+// let usersURL = /api/v1.0/users
+// let reviewsURL = /api/v1.0/reviews 
+
+function displayBubbleChart(){
+    // you may need to install Chart.js 'npm install chart.js' or may not
+    let booksURL = '/api/v1.0/books';
+    fetch(booksURL)
+    .then(response => response.json()) //Parse the repsonse to Json
+    .then(booksData => {
+        console.log(booksData)
+        let authors_list=booksData.map(item=>item['Book-Author'])
+        let Year_Publication=booksData.map(item=>item['Year-Of-Publication'])
+        const ctx = document.getElementById('chartJsChart');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+              labels: authors_list,
+              datasets: [{
+                label: 'ReviewScore',
+                data: Year_Publication,
+                borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true
+                }
+              }
+            }
+          });
+    })    
+}
+
+displayBubbleChart()
+ 
